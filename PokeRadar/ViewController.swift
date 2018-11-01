157a55fcf3468793dffea3f771d0c3b8fdc26fa6
Cristian <chris.sedano.arenas@gmail.com>
2018-11-01 16:49:41 +0000

    New Project
    Implement Map Kit View and button in Main.storyboard, and import MapKit and var mapView and @IBAction func reportPokemon

---
+++ PokeRadar/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  PokeRadar
+//
+//  Created by Cristian Sedano Arenas on 1/11/18.
+//  Copyright © 2018 Cristian Sedano Arenas. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+    @IBAction func reportPokemon(_ sender: UIButton) {
+    }
+}
+
