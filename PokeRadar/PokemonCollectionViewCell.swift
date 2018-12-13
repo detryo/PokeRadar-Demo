a317776231fa0190fc330defa43657f36340343a
Cristian <chris.sedano.arenas@gmail.com>
2018-12-13 12:16:42 +0000

    PokemonCollectionViewController.swift & PokemonCollectionViewCell.swift
    Implemnet two new Cocoa Touch Class, in Main.storyboard add CollectionView Controller and Navigation View, Bar Button Item, two Label and Image View, and configPokemonCollectionViewController.swift and PokemonCollectionViewCell.swift
    
     Please enter the commit message for your changes. Lines starting

---
+++ PokeRadar/PokemonCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  PokemonCollectionViewCell.swift
+//  PokeRadar
+//
+//  Created by Cristian Sedano Arenas on 13/12/18.
+//  Copyright © 2018 Cristian Sedano Arenas. All rights reserved.
+//
+
+import UIKit
+
+class PokemonCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var pokemonImageView: UIImageView!
+    @IBOutlet weak var pokemonNumber: UILabel!
+    @IBOutlet weak var pokemonName: UILabel!
+}
